cd803ce809e66ff836e1ad6b07e924ccaed8ee7f
arvin <2537507352@qq.com>
2024-12-11 17:11:10 +0800

    chore: Add GitHub Actions for deployment

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: "/lucky-design",
+  publicPath: "/lucky-design",
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'lucky-design',

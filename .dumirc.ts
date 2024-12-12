4a587849457693d4393e795b1f660b76130a50ab
arvin <2537507352@qq.com>
2024-12-12 10:52:26 +0800

    chore: update .dumiirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '/lucky-design', // 设置站点部署路径
+  publicPath: '/lucky-design/', // 设置资源部署路径
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'lucky-design',
